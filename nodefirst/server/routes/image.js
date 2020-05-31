e1ec6307d8cc3b832a251aa3889eb434b3cb0a89
Qing <zhangqingqing288@gmail.com>
2020-05-31 23:02:01 +0800

    fix

--- nodefirst/server/routes/image.js
+++ nodefirst/server/routes/image.js
@@ -17,11 +17,9 @@ var upload = multer({ storage: storage })//当前目录下建立文件夹uploads
 
 router.post('/upload', upload.array('img',1), function(req, res, next) {//这里的 "img" 字段要和前端 ForData.append 中的字段一致
   console.log('upload image success');
-  res.json({result:"success"});
-
-  // convertImageByPython(e=>{
-  //   res.json({result:"success"});
-  // });
+  convertImageByPython(e=>{
+    res.json({result:"success"});
+  });
 });
 
 router.get('/editImg/*',(req,res,next)=>{
